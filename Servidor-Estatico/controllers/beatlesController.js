9c9424a982c7f7f15123504ce6d4f61a29529ef3
Mauricio Fuentes <mauriciofb@gmail.com>
2024-11-27 22:15:03 -0300

    (wip): Beatles CRUD

--- Servidor-Estatico/controllers/beatlesController.js
+++ Servidor-Estatico/controllers/beatlesController.js
@@ -3,6 +3,7 @@
  */
 
 import { BeatlesModel } from "../models/beatlesModel.js"
+import * as crypto from 'node:crypto'
 
 export const beatlesController = async (req, res, payloadBruto, urlparts) => {
   /**
@@ -22,6 +23,7 @@ export const beatlesController = async (req, res, payloadBruto, urlparts) => {
   }
   /**
    * Mostrar disco por ID
+   * /api/beatles/:id
    */
   else if (req.method == 'GET' && urlparts[2] && urlparts.length <= 3 ) {
     let disco = await BeatlesModel.getById(urlparts[2])
@@ -37,11 +39,28 @@ export const beatlesController = async (req, res, payloadBruto, urlparts) => {
   /**
    * Mostrar disco por nombre
    */
-  
+
   /**
    * Crear discos
+   * 
+   * /api/beatles /...
    */
+  else if(req.method = 'POST' && !urlparts[2]) {
+    try {
+      let disco = JSON.parse(payloadBruto)
+      let id = crypto.randomUUID()
 
+      /**
+       * 1- Leer el archivo
+       * 2- Agregar disco con id
+       * 3- Guardar data en el archivo
+       * 4- Enviar respuesta de éxito
+       */
+    } catch (err) {
+      res.writeHead(400, 'Bad Request', { "content-type": "application/json" })
+      res.end(JSON.stringify({ message: 'Solicitud mal hecha'}))
+    }
+  }
   /**
    * Actualizar discos
    */
